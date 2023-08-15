697dca0e684262e8d9c07ea66b85c5082468de78
nsp1919 <110881782+nsp1919@users.noreply.github.com>
2023-08-15 21:14:26 +0530

    update

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+//        obj.name="sai";
+//        name has private access in com.driver.RWOnly
+        obj.setName("sai");
+        obj.getName();
+
+    }
   
 }
